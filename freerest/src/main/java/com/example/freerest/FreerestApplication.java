e31db3752f5fda62d0c1323e961b220b274388e4
like_ <like_@DESKTOP-J66FP9M>
2024-02-04 15:13:20 +0530

    freerest testok

---
+++ freerest/src/main/java/com/example/freerest/FreerestApplication.java
@@ -0,0 +1,13 @@
+package com.example.freerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreerestApplication.class, args);
+	}
+
+}
---
+++ freerest/src/test/java/com/example/freerest/FreerestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
